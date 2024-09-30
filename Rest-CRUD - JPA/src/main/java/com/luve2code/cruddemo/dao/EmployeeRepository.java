3e1efd4416e3bc98225378b1d05493716bc25800
HoOnn <148750499+Ho0nn@users.noreply.github.com>
2024-09-30 22:15:02 +0300

    Until final touch

---
+++ Rest-CRUD - JPA/src/main/java/com/luve2code/cruddemo/dao/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.luve2code.cruddemo.dao;
+import com.luve2code.cruddemo.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
+
+
+}
