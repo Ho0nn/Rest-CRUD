4c59d8f98fb1fbd75402589e84e818fde9a0ba89
HoOnn <148750499+Ho0nn@users.noreply.github.com>
2024-10-03 22:08:05 +0300

    JDBC Security

--- Rest-CRUD/src/main/java/com/luve2code/cruddemo/security/DemoSecurityConfig.java
+++ Rest-CRUD/src/main/java/com/luve2code/cruddemo/security/DemoSecurityConfig.java
@@ -1,6 +1,4 @@
 package com.luve2code.cruddemo.security;
-
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -9,12 +7,60 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DemoSecurityConfig {
 
+    //add support for JDBC...no more hardcode users
+
     @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource){
+
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+
+        // define query to retrieve a user by username
+        jdbcUserDetailsManager.setUsersByUsernameQuery(
+                "select user_id , pw , active from members where user_id=?"
+        );
+
+        // define query to retrieve the authorities/roles by username
+        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery(
+                "select user_id , role from roles where user_id=? "
+        );
+
+        return jdbcUserDetailsManager;
+    }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(configurer ->
+                configurer.requestMatchers(HttpMethod.GET,"/api/employees").hasRole("EMPLOYEE")
+                .requestMatchers(HttpMethod.GET,"/api/employees/**").hasRole("EMPLOYEE")
+                .requestMatchers(HttpMethod.POST,"/api/employees").hasRole("MANAGER")
+                .requestMatchers(HttpMethod.PUT,"/api/employees").hasRole("MANAGER")
+                .requestMatchers(HttpMethod.DELETE,"/api/employees/**").hasRole("ADMIN")
+        );
+
+        // use HTTp Basic authentication
+        http.httpBasic(Customizer.withDefaults());
+        // disable cross site request forgery(CSRF)
+        http.csrf(csrf->csrf.disable());
+        return http.build();
+    }
+
+
+
+
+
+
+
+
+
+ /*   @Bean
     public InMemoryUserDetailsManager userDetailsManager(){
         UserDetails hanin = User.builder()
                 .username("hanin")
@@ -33,23 +79,5 @@ public class DemoSecurityConfig {
                 .build();
 
         return new InMemoryUserDetailsManager(hanin,ahmed,mohamed);
-    }
-
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(configurer ->
-                configurer.requestMatchers(HttpMethod.GET,"/api/employees").hasRole("EMPLOYEE")
-                .requestMatchers(HttpMethod.GET,"/api/employees/**").hasRole("EMPLOYEE")
-                .requestMatchers(HttpMethod.POST,"/api/employees").hasRole("MANAGER")
-                .requestMatchers(HttpMethod.PUT,"/api/employees").hasRole("MANAGER")
-                .requestMatchers(HttpMethod.DELETE,"/api/employees/**").hasRole("ADMIN")
-        );
-
-        // use HTTp Basic authentication
-        http.httpBasic(Customizer.withDefaults());
-        // disable cross site request forgery(CSRF)
-        http.csrf(csrf->csrf.disable());
-
-        return http.build();
-    }
+    }*/
 }
